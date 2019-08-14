35c5b487198d1d5d502ce7c2f786c4157a90b201
Rone Cesar <ronecesar@tilix.com.br>
2019-08-13 21:45:48 -0300

    Adjust user routes

--- controllers/users.js
+++ controllers/users.js
@@ -1,130 +1,141 @@
-const users = require("../models/users")
+const users = require("../models/users");
 
 exports.list = (req, res) => {
-    users.list()
+  users
+    .list()
     .then(data => {
-        res.status(200).json({
-            success: true,
-            data: data
-        })
+      res.status(200).json({
+        success: true,
+        data: data
+      });
     })
     .catch(error => {
-        res.status(500).json({
-            success: false,
-            message: error
-        })
-    })
-}
+      res.status(500).json({
+        success: false,
+        message: error
+      });
+    });
+};
 
 exports.create = (req, res) => {
-    const body = req.body
+  const body = req.body;
 
-    users.findOne(body.email)
+  users
+    .findOne(body.email)
     .then(student => {
-        if(!student) {
-            users.create(body)
-            .then(() => {
-                res.status(200).json({
-                    success: true,
-                })
-            })
-            .catch(error => {
-                console.log(error);
-                res.status(500).json({
-                    success: false,
-                    message: error
-                })
-            })
+      if (!student) {
+        users
+          .create(body)
+          .then(() => {
+            res.status(200).json({
+              success: true
+            });
+          })
+          .catch(error => {
+            console.log(error);
+            res.status(500).json({
+              success: false,
+              message: error
+            });
+          });
 
-            return
-        }
+        return;
+      }
 
-        res.status(500).json({
-            success: false,
-            code: "user_exists",
-            message: 'Usuário já cadastrado'
-        })
+      res.status(500).json({
+        success: false,
+        code: "user_exists",
+        message: "Usuário já cadastrado"
+      });
     })
     .catch(error => {
-        console.log("ERROR", error)
-        res.status(500).json({
-            success: false,
-            message: error
-        })
-    })
-
-}
+      console.log("ERROR", error);
+      res.status(500).json({
+        success: false,
+        message: error
+      });
+    });
+};
 
 //TODO alter method
 exports.update = (req, res) => {
-    const student_id = req.params.id
-    const body = req.body
+  const student_id = req.params.id;
+  const body = req.body;
 
-    let data = {}
+  let data = {};
 
-    if(body.name) { data.name = body.name }
-    if(body.birth_date) { data.birth_date = body.birth_date }
-    if(body.phone) { data.phone = body.phone }
+  if (body.name) {
+    data.name = body.name;
+  }
+  if (body.birth_date) {
+    data.birth_date = body.birth_date;
+  }
+  if (body.phone) {
+    data.phone = body.phone;
+  }
 
-    users.findOne(student_id)
+  users
+    .findOne(student_id)
     .then(student => {
-        if(student) {
-            users.update(student_id, data)
-            .then(() => {
-                res.status(200).json({
-                    success: true,
-                })
-            })
-            .catch(error => {
-                res.status(500).json({
-                    success: false,
-                    message: error
-                })
-            })
+      if (student) {
+        users
+          .update(student_id, data)
+          .then(() => {
+            res.status(200).json({
+              success: true
+            });
+          })
+          .catch(error => {
+            res.status(500).json({
+              success: false,
+              message: error
+            });
+          });
 
-            return
-        }
+        return;
+      }
 
-        res.status(404).json({
-            success: false,
-            code: "user_not_found",
-            message: 'Usuário não encontrado'
-        })
+      res.status(404).json({
+        success: false,
+        code: "user_not_found",
+        message: "Usuário não encontrado"
+      });
     })
     .catch(error => {
-        res.status(500).json({
-            success: false,
-            message: error
-        })
-    })
-}
+      res.status(500).json({
+        success: false,
+        message: error
+      });
+    });
+};
 
 exports.get = (req, res) => {
-    const student_id = req.params.id
+  const email = req.params.id;
 
-    console.log(student_id)
-        
-    users.findOne(student_id)
+  console.log(email);
+
+  users
+    .findOne(email)
     .then(result => {
-        if(result) {
-            res.status(200).json({
-                success: true,
-                data: result
-            })
+      if (result) {
+        res.status(200).json({
+          success: true,
+          data: result
+        });
 
-            return;
-        }
+        return;
+      }
 
-        res.status(404).json({
-            success: false,
-            code: "user_not_found",
-            message: "Usuário não encontrado"
-        })
+      res.status(404).json({
+        success: false,
+        code: "user_not_found",
+        message: "Usuário não encontrado"
+      });
     })
     .catch(error => {
-        res.status(500).json({
-            success: false,
-            message: error
-        })
-    })
-}
+      res.status(500).json({
+        success: false,
+        message: error
+      });
+    });
+};
