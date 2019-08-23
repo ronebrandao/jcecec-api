a413ec779988bfb979cabb0d6b0758a047b65315
Rone Cesar <ronecesar@tilix.com.br>
2019-08-23 01:12:18 -0300

    Change user update key

--- models/users.js
+++ models/users.js
@@ -1,74 +1,78 @@
-const knex = require('../config/knex').knex
-const moment = require("moment")
+const knex = require("../config/knex").knex;
+const moment = require("moment");
 
-function findOne(email) {    
-    return new Promise(async (resolve, reject) => {
-        await knex("user").where({email: email}).first()
-        .then(data => {
-            resolve(data)
-        })
-        .catch(error => {
-            reject(error)
-        })
-    })
+function findOne(email) {
+  return new Promise(async (resolve, reject) => {
+    await knex("user")
+      .where({ email: email })
+      .first()
+      .then(data => {
+        resolve(data);
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
 }
 
 function list() {
-    return new Promise(async (resolve, reject) => {
-        await knex("user")
-        .then(data => {
-            resolve(data)
-        })
-        .catch(error => {
-            reject(error)
-        })
-    })
+  return new Promise(async (resolve, reject) => {
+    await knex("user")
+      .then(data => {
+        resolve(data);
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
 }
 
 function create(data) {
-    return new Promise(async (resolve, reject) => {
-        await knex("user")
-        .insert({
-            name: data.firstName,
-            family_name: data.lastName,   
-            email: data.email,
-            birth_date: data.birthDate,
-            institution: data.institution,
-            type: data.type,
-            subscribed: data.subscribed,
-            cep: data.cep,
-            street: data.street,
-            street_number: data.streetNumber,
-            neighborhood: data.neighborhood,
-            state: data.state,
-            city: data.city,
-            complement: data.complement,
-            created_at: moment().format('YYYY-MM-DD HH:mm:ss')
-        })
-        .then(data => {
-            resolve(true)
-        })
-        .catch(error => {
-            reject(error)
-        })
-    })
+  return new Promise(async (resolve, reject) => {
+    await knex("user")
+      .insert({
+        name: data.firstName,
+        family_name: data.lastName,
+        email: data.email,
+        birth_date: data.birthDate,
+        institution: data.institution,
+        type: data.type,
+        subscribed: data.subscribed,
+        cep: data.cep,
+        street: data.street,
+        street_number: data.streetNumber,
+        neighborhood: data.neighborhood,
+        state: data.state,
+        city: data.city,
+        complement: data.complement,
+        created_at: moment().format("YYYY-MM-DD HH:mm:ss")
+      })
+      .then(data => {
+        resolve(true);
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
 }
 
-function update(id, data) {
-    return new Promise(async (resolve, reject) => {
-        await knex("user").where({id: id}).update(data)
-        .then(data => {
-            resolve(data)
-        })
-        .catch(error => {
-            reject(error)
-        })
-    })
+function update(email, data) {
+  return new Promise(async (resolve, reject) => {
+    await knex("user")
+      .where({ email: email })
+      .update(data)
+      .then(data => {
+        resolve(data);
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
 }
 
 module.exports = {
-    findOne: findOne,
-    list: list,
-    create: create,
-    update: update
-}
+  findOne: findOne,
+  list: list,
+  create: create,
+  update: update
+};
