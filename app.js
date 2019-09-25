8bb5954d1f2c2d84ef429592a2c71c26d73ad231
Rone <rone_filho@hotmail.com>
2019-09-25 14:06:46 -0300

    Add cognito authentication

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const CognitoExpress = require("cognito-express");
 
 const app = express();
 
@@ -13,6 +14,31 @@ const users = require("./routes/users");
 const institutions = require("./routes/institutions");
 const submissions = require("./routes/submissions");
 const proofreads = require("./routes/proofreads");
+
+authenticationRoute = express.Router();
+
+app.use("/", authenticationRoute);
+
+const cognitoExpress = new CognitoExpress({
+  region: "us-east-2",
+  cognitoUserPoolId: process.env.COGNITO_USER_POOL_ID,
+  tokenUse: "access",
+  tokenExpiration: 3600000
+});
+
+authenticationRoute.use(function(req, res, next) {
+  let accessTokenFromClient = req.headers.authorization;
+
+  if (!accessTokenFromClient)
+    return res.status(401).send("Access Token missing from header");
+
+  cognitoExpress.validate(accessTokenFromClient, function(err, response) {
+    if (err) return res.status(401).send(err);
+
+    res.locals.user = response;
+    next();
+  });
+});
 
 app.use("/users", users);
 app.use("/institutions", institutions);
